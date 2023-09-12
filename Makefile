c5dab7ac1cef6ba2b7675dec3bb60e3363678010
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-09-12 15:54:51 +0200

    makefile arreglado y completo

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
-SRC = ft_*.c
+SRC = ft_bzero.c ft_isalnum.c ft_isdigit.c
 NAME = libft.a
 OBJ = $(SRC:.c=.o)
 FLAGS = -Wall -Wextra -Werror
 
 all:$(NAME)
 
-$(NAME):
-	cc $(FLAGS) $(SRC) -o $(NAME)
-
+$(NAME): $(OBJ)
+	ar rcs $(NAME) $(OBJ)
+	
+%.o : %.c
+	gcc $(FLAGS) -c $< -o $@
 clean:
 	rm -f $(OBJ)
 fclean: clean
-		rm -f $(NAME) $(OBJ)
+	rm -f $(NAME) $(OBJ)
 
-re: all fclean
+re: fclean all
+
+.PHONY: clean fclean re
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albertoguzman <albertoguzman@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/12 11:22:15 by albertoguzm       #+#    #+#             */
-/*   Updated: 2023/09/12 13:30:27 by albertoguzm      ###   ########.fr       */
+/*   Created: 2023/09/12 13:30:49 by albertoguzm       #+#    #+#             */
+/*   Updated: 2023/09/12 13:39:16 by albertoguzm      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_bzero(void *s, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    size_t  i;
-    char    *ptr;
-
-    i = 0;
-    ptr = (char *)s;
-    while (i < n)
-    {
-        ptr[i] = 0;
-        i++;
-    }
+	
 }
-
-// int main(void)
-// {
-//     size_t  n;
-//     char    str[] = "hola";
-
-//     n = 4;
-//     ft_bzero(str, n);
-
-//     printf("%s", str);
-
-//     return (0);
-// }
