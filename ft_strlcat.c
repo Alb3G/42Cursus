c18d408df66d2ec9c2cdcc24fe34cf253e6c646f
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-09-13 19:33:33 +0200

    strlcat no funca

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: albertoguzman <albertoguzman@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 15:47:55 by albguzma          #+#    #+#             */
-/*   Updated: 2023/09/13 18:48:05 by albertoguzm      ###   ########.fr       */
+/*   Updated: 2023/09/13 19:29:38 by albertoguzm      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
 	size_t	src_len;
 	size_t	dst_len;
-	size_t	i;
-	size_t	dst_len2;
+	size_t	dst_len_mod;
 
-	i = 0;
-	src_len = ft_strlen(src);
+
 	dst_len = ft_strlen(dst);
-	dst_len2 = ft_strlen(dst);
-	// if (dstsize == 0)
-	// {
-	// 	ft_strlcpy(dst, src, dstsize);
-	// 	return (src_len + dst_len);
-	// }
+	src_len = ft_strlen(src);
+	dst_len_mod = ft_strlen(dst);
+	i = 0;
+	if (dstsize == 0)
+		return (src_len + dst_len);
 	while (src[i] != '\0' && i < dstsize - 1)
 	{
-		dst[dst_len2] = src[i];
-		dst_len2++;
+		dst[dst_len_mod] = src[i];
+		dst_len_mod++;
 		i++;
 	}
+	dst[dst_len_mod] = '\0';
 	return (src_len + dst_len);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: albertoguzman <albertoguzman@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:25:25 by albguzma          #+#    #+#             */
-/*   Updated: 2023/09/13 18:32:48 by albertoguzm      ###   ########.fr       */
+/*   Updated: 2023/09/13 19:31:23 by albertoguzm      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 int	main(void)
 {
 	char	src[] = "hello";
-	char	dst[10] = "al";
+	char	dst[10] = "     ";
+	char	dst2[10] = "     ";
 
 	printf("Res del strlcat: %li\n", strlcat(dst, src, 8));
 	printf("%s\n", dst);
 
-	printf("Res del ft_strlcat: %li\n", ft_strlcat(dst, src, 8));
-	printf("%s", dst);
+	printf("Res del ft_strlcat: %li\n", ft_strlcat(dst2, src, 8));
+	printf("%s", dst2);
 
 	return (0);
 }
