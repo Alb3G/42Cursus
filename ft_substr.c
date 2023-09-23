61fcbe50827d03742bbd0fe89cf23e5b696d9d27
Alberto Guzman Moreno <albguzma@c3r9s6.42malaga.com>
2023-09-23 18:42:05 +0200

    voy por substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albguzma <albguzma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/23 17:18:24 by albguzma          #+#    #+#             */
-/*   Updated: 2023/09/23 18:15:25 by albguzma         ###   ########.fr       */
+/*   Created: 2023/09/23 18:17:49 by albguzma          #+#    #+#             */
+/*   Updated: 2023/09/23 18:41:41 by albguzma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
+	size_t	i;
+	char	*substring;
 
-	str = (char *)malloc(ft_strlen(s1) + 1 * sizeof(char));
-	if (str == 0)
+	i = 0;
+	substring = (char *)malloc(ft_strlen(s) + 1 * sizeof(char));
+	if (substring == 0)
 		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
-	return (str);
+	while (i < len)
+	{
+		substring[i] = s[start];
+		i++;
+		start++;
+	}
+	return (substring);
 }
